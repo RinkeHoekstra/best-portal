cc23b51d900aba8ce00589bb17a7b13f4ce07311
(no author) <(no author)@858cfcfd-281a-4d2c-9507-69aafe8929a6>
2009-05-29 12:34:57 +0000

    Added DISTINCT to SELECT queries

--- getmapping.php
+++ getmapping.php
@@ -35,14 +35,14 @@ $sc->tellSesame($turtle,$context);
 $cl = new ConceptList();
 $ns = new Namespaces();
 
-$sparql_query = $ns->sparql."SELECT ?concept ?label WHERE { ?concept best:describes ".$query_instance." . ?concept skos:inScheme tv:tort-scheme . ?concept skos:prefLabel ?label .}";
+$sparql_query = $ns->sparql."SELECT DISTINCT ?concept ?label WHERE { ?concept best:describes ".$query_instance." . ?concept skos:inScheme tv:tort-scheme . ?concept skos:prefLabel ?label .}";
 
 // print "<pre>".htmlentities($sparql_query)."</pre>";
 	print "<h5>Legal Terms</h5>\n";
 $cl->getDiv($sparql_query);
 
 
-$sparql_query = $ns->sparql."SELECT ?concept ?label WHERE { ?concept best:describes ".$query_instance." . ?concept skos:inScheme tv:tort-scheme .  {?concept skos:altLabel ?label} UNION {?concept skos:prefLabel ?label} .}";
+$sparql_query = $ns->sparql."SELECT DISTINCT ?concept ?label WHERE { ?concept best:describes ".$query_instance." . ?concept skos:inScheme tv:tort-scheme .  {?concept skos:altLabel ?label} UNION {?concept skos:prefLabel ?label} .}";
 
 // print "<pre>".htmlentities($sparql_query)."</pre>";
 	print "<h5>Generated Queries</h5>\n";
@@ -51,14 +51,14 @@ $sparql_query = $ns->sparql."SELECT ?concept ?label WHERE { ?concept best:descri
 	$cl->getQueryString($sparql_query,"tort query");
 	print "</div>";
 	
-$sparql_query = $ns->sparql."SELECT ?concept ?label WHERE { ?concept best:describes ".$query_instance." . {?concept skos:altLabel ?label} UNION {?concept skos:prefLabel ?label} .}";
+$sparql_query = $ns->sparql."SELECT DISTINCT ?concept ?label WHERE { ?concept best:describes ".$query_instance." . {?concept skos:altLabel ?label} UNION {?concept skos:prefLabel ?label} .}";
 
 // print "<pre>".htmlentities($sparql_query)."</pre>";
 	print "<div class='solrquery'>";
 	$cl->getQueryString($sparql_query,"mixed query");
 	print "</div>";
 
-$sparql_query = $ns->sparql."SELECT ?concept ?label ?weight WHERE { ?concept best:describes ".$query_instance." . ?concept skos:inScheme tv:tort-scheme .  ?concept to:fingerprint ?fp . ?fp to:value ?label . ?fp to:weight ?weight .}";
+$sparql_query = $ns->sparql."SELECT DISTINCT ?concept ?label ?weight WHERE { ?concept best:describes ".$query_instance." . ?concept skos:inScheme tv:tort-scheme .  ?concept to:fingerprint ?fp . ?fp to:value ?label . ?fp to:weight ?weight .}";
 
 	print "<div class='solrquery'>";
 	$cl->getWeightedQueryString($sparql_query,"weighed tort query");
