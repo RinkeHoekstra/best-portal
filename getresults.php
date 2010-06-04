b62f7388d7f0f510ae81dae6060eaa4e741945f4
hoekstra <hoekstra@858cfcfd-281a-4d2c-9507-69aafe8929a6>
2010-06-04 10:14:11 +0000

    Fixed bug caused by bugfix

--- getresults.php
+++ getresults.php
@@ -34,7 +34,6 @@ if ($solr_results){
 	// print $results->response->numFound;
 
 	$times = array();
-	if($solr_results->response->docs.count > 0){
 	foreach ($solr_results->response->docs as $doc) {
 		$rnl_ljn =  $doc->rnl_ljn;
 		$ljn = $doc->ljn;
@@ -137,7 +136,7 @@ if ($solr_results){
 
 	}
 	$json_courts = json_encode($courts_array);
-	}
+	
 }
 
 $json_results = '{"query":"'.urlencode($query).'","solr":'.$json_solr_results.',"timeline":'.$json_timeline.',"latestdate":"'.$latest_date.'","places":'.$json_places.',"courts":'.$json_courts.'}';
